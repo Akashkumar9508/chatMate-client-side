7611a31da55f5c8be20c9c0d2292ee0500cf25f2
Mantu Kumar <mantu911711@gmail.com>
2024-11-10 21:39:44 +0530

    Chat Mate project initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className="h-screen w-full flex justify-center items-center bg-slate-500 text-5xl text-orange-400">Chat-Mate</p>
+      </div>
+    </>
+  )
+}
+
+export default App
