7bd6b7b7f6d56912924bcc2ed7c11943367d63ff
Anant Kumar <anant1042005@gmail.com>
2025-01-18 14:28:23 +0530

    UI changes for mobile

--- src/components/Chatheader.jsx
+++ src/components/Chatheader.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const ChatHeader = ({ openSidebar }) => {
     const selctedUser = useSelector((state) => state.user?.selectedUser);
     return (
-        <div className="flex items-center justify-between mt-4 5 ml-4 text-center " >
+        <div className="flex items-center justify-between mt-4 5 px-4 text-center " >
             <div>
                 <NavLink className="flex items-center px-2 gap-3 cursor-pointer" to={`/user/${selctedUser?.userName}`}>
                     <img src={selctedUser?.avatar} className={`h-12 w-12 ${selctedUser ? "" : "hidden"}`} />
